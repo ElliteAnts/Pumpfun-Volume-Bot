74463f939d14fb5d5dc637dc7834a31cec7041f7
metadotgg <matthiastovar.sr@gmail.com>
2026-02-01 02:43:06 -0800

    extend to token 2022

--- gather.ts
+++ gather.ts
@@ -6,167 +6,215 @@ import {
   Transaction,
   TransactionInstruction,
   sendAndConfirmTransaction,
+  LAMPORTS_PER_SOL,
+  PublicKey,
 } from '@solana/web3.js';
 import {
-  TOKEN_PROGRAM_ID,
   createAssociatedTokenAccountIdempotentInstruction,
   createCloseAccountInstruction,
   createTransferCheckedInstruction,
+  getAccount,
   getAssociatedTokenAddress,
+  getAssociatedTokenAddressSync,
 } from '@solana/spl-token';
-import { SPL_ACCOUNT_LAYOUT, TokenAccount } from '@raydium-io/raydium-sdk';
 import base58 from 'bs58';
 
-import { readJson, saveNewFile, sleep } from './utils';
-import { execute } from './executor/legacy';
-import { PRIVATE_KEY, RPC_ENDPOINT, RPC_WEBSOCKET_ENDPOINT } from './constants';
-import { makeSellPumpfunTokenTx } from './utils/pumpfun';
+import { Data, readJson, saveNewFile, sleep } from './service';
+import { PRIVATE_KEY, RPC_ENDPOINT, RPC_WEBSOCKET_ENDPOINT, TOKEN_MINT } from './constants';
+import { PUMPFUN_CONSTANTS } from './constants/pumpfunConstants';
 
 const connection = new Connection(RPC_ENDPOINT, { wsEndpoint: RPC_WEBSOCKET_ENDPOINT, commitment: 'confirmed' });
 const mainKp = Keypair.fromSecretKey(base58.decode(PRIVATE_KEY));
 
-const main = async () => {
-  const walletsData = readJson();
+export const gather = async () => {
+  const walletsData: Data[] = readJson();
+  // export const gather = async (walletsData: Data[]) => {
+  const wallets: Keypair[] = walletsData.map((wallet: Data) => Keypair.fromSecretKey(base58.decode(wallet.privateKey)));
 
-  const wallets = walletsData.map(({ privateKey }) => Keypair.fromSecretKey(base58.decode(privateKey)));
-  wallets.map(async (kp, i) => {
+  wallets.map(async (wallet: Keypair, i: number) => {
     try {
-      const solBalance = await connection.getBalance(kp.publicKey);
-      if (solBalance > 0)
-        console.log('Wallet ', kp.publicKey.toBase58(), ' SOL balance is ', (solBalance / 10 ** 9).toFixed(4));
-      await sleep(i * 50);
-      const accountInfo = await connection.getAccountInfo(kp.publicKey);
+      const walletPublicKey = wallet.publicKey;
+      // const solBalance = (await connection.getBalance(walletPublicKey)) / LAMPORTS_PER_SOL;
 
-      const tokenAccounts = await connection.getTokenAccountsByOwner(
-        kp.publicKey,
-        {
-          programId: TOKEN_PROGRAM_ID,
-        },
-        'confirmed',
-      );
-      const ixs: TransactionInstruction[] = [];
-      const accounts: TokenAccount[] = [];
+      while (true) {
+        const solBalance = await connection.getBalance(walletPublicKey);
 
-      if (tokenAccounts.value.length > 0)
-        for (const { pubkey, account } of tokenAccounts.value) {
-          accounts.push({
-            pubkey,
-            programId: account.owner,
-            accountInfo: SPL_ACCOUNT_LAYOUT.decode(account.data),
-          });
+        if (solBalance <= 0) {
+          console.log(`${walletPublicKey.toBase58()} has no SOL`);
+          break;
         }
 
-      for (let j = 0; j < accounts.length; j++) {
-        const baseAta = await getAssociatedTokenAddress(accounts[j].accountInfo.mint, mainKp.publicKey);
-        const tokenAccount = accounts[j].pubkey;
-        const tokenBalance = (await connection.getTokenAccountBalance(accounts[j].pubkey)).value;
-        console.log('Token balance : ', tokenBalance.uiAmount);
+        console.log(`${walletPublicKey.toBase58()} has ${solBalance / LAMPORTS_PER_SOL} SOL`);
 
-        let i = 0;
-        while (true) {
-          if (i > 5) {
-            console.log('Sell error before gather');
-            break;
-          }
-          if (tokenBalance.uiAmount == 0) {
-            break;
-          }
-          try {
-            const sellTx = await makeSellPumpfunTokenTx(kp, accounts[j].accountInfo.mint);
-            if (sellTx == null) {
-              throw new Error('Error getting sell tx');
+        // const accountInfo = await connection.getAccountInfo(walletPublicKey);
+        // const tokenAccounts = await connection.getTokenAccountsByOwner(
+        //   walletPublicKey,
+        //   {
+        //     programId: PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+        //   },
+        //   'confirmed',
+        // );
+
+        let ixs: TransactionInstruction[] = [];
+        // const accounts: TokenAccount[] = [];
+
+        // if (tokenAccounts.value.length < 0) return;
+
+        // for (const { pubkey, account } of tokenAccounts.value) {
+        //   accounts.push({
+        //     pubkey,
+        //     programId: account.owner,
+        //     accountInfo: SPL_ACCOUNT_LAYOUT.decode(account.data),
+        //   });
+        // }
+
+        // for (let j = 0; j < accounts.length; j++) {
+        const baseAta = await getAssociatedTokenAddress(
+          new PublicKey(TOKEN_MINT),
+          mainKp.publicKey,
+          false,
+          PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+        );
+
+        const associatedTokenAccount = getAssociatedTokenAddressSync(
+          new PublicKey(TOKEN_MINT),
+          wallet.publicKey,
+          false,
+          PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+        );
+
+        // const tokenAccount = accounts[j].pubkey;
+        // console.log("tokenAccount:", tokenAccount.toBase58())
+        try {
+          const tokenBalance = (await connection.getTokenAccountBalance(associatedTokenAccount)).value;
+          if (tokenBalance.uiAmount && tokenBalance.uiAmount > 0) {
+            console.log(`${wallet.publicKey.toBase58()} has ${tokenBalance.uiAmount} tokens after sell`);
+            try {
+              const tokenInfo = await getAccount(
+                connection,
+                baseAta,
+                'confirmed',
+                PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+              );
+              console.log('Token ATA exists on main wallet');
+            } catch (error) {
+              ixs.push(
+                createAssociatedTokenAccountIdempotentInstruction(
+                  mainKp.publicKey,
+                  baseAta,
+                  mainKp.publicKey,
+                  new PublicKey(TOKEN_MINT),
+                  PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+                ),
+              );
             }
 
-            const simResult = await connection.simulateTransaction(sellTx);
+            console.log('Token Transfer Instruction');
 
-            if (simResult.value.err) {
-              console.log('Transaction simulation failed');
-              console.log('Error : ', simResult);
-              break;
-            }
-            const latestBlockhashForSell = await connection.getLatestBlockhash();
-            const txSellSig = await execute(sellTx, latestBlockhashForSell, false);
-            const tokenSellTx = txSellSig ? `https://solscan.io/tx/${txSellSig}` : '';
-            console.log('Sold token, ', tokenSellTx);
-            break;
-          } catch (error) {
-            i++;
+            ixs.push(
+              createTransferCheckedInstruction(
+                associatedTokenAccount,
+                new PublicKey(TOKEN_MINT),
+                baseAta,
+                wallet.publicKey,
+                BigInt(tokenBalance.amount),
+                tokenBalance.decimals,
+                undefined,
+                PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+              ),
+            );
           }
+
+          console.log(`Close Token Ata instruction`);
+          const closeAtaIx = createCloseAccountInstruction(
+            associatedTokenAccount,
+            wallet.publicKey,
+            wallet.publicKey,
+            undefined,
+            PUMPFUN_CONSTANTS.TOKEN_2022_PROGRAM_ID,
+          );
+          ixs.push(closeAtaIx);
+        } catch (error) {
+          console.log(`Token ATA does not exist on ${wallet.publicKey.toBase58()}`);
         }
-        await sleep(1000);
+        // let i = 0;
+        // while (true) {
+        //   if (tokenBalance.uiAmount == 0) break;
 
-        const tokenBalanceAfterSell = (await connection.getTokenAccountBalance(accounts[j].pubkey)).value;
-        if (tokenBalanceAfterSell.uiAmount && tokenBalanceAfterSell.uiAmount > 0) {
-          console.log('Token Balance After Sell:', kp.publicKey.toBase58(), tokenBalanceAfterSell.amount);
-          ixs.push(
-            createAssociatedTokenAccountIdempotentInstruction(
-              mainKp.publicKey,
-              baseAta,
-              mainKp.publicKey,
-              accounts[j].accountInfo.mint,
-            ),
-          );
-          ixs.push(
-            createTransferCheckedInstruction(
-              tokenAccount,
-              accounts[j].accountInfo.mint,
-              baseAta,
-              kp.publicKey,
-              BigInt(tokenBalanceAfterSell.amount),
-              tokenBalance.decimals,
-            ),
-          );
-        } else ixs.push(createCloseAccountInstruction(tokenAccount, mainKp.publicKey, kp.publicKey));
-      }
+        //   if (i > 5) {
+        //     console.log('Sell error before gather');
+        //     break;
+        //   }
 
-      if (accountInfo) {
-        const solBal = await connection.getBalance(kp.publicKey);
+        //   try {
+        //     const sellTx = await makeSellPumpfunTokenTx(wallet, new PublicKey(TOKEN_MINT));
+
+        //     if (sellTx == null) {
+        //       throw new Error('Error in making pumpfun sell transaction');
+        //     }
+
+        //     const simResult = await connection.simulateTransaction(sellTx);
+
+        //     if (simResult.value.err) {
+        //       console.log('pumpfun sell transaction simulation error: ', simResult);
+        //       throw new Error('Error in making pumpfun sell transaction');
+        //     }
+
+        //     const latestBlockhash = await connection.getLatestBlockhash();
+        //     const sellTxSignature = await execute(sellTx, latestBlockhash, false);
+        //     console.log(`sellTxLink: https://solscan.io/tx/${sellTxSignature ?? ''})`);
+        //     break;
+        //   } catch (error) {
+        //     console.log('Error:', error);
+        //     i++;
+        //   }
+        // }
+
+        // const tokenBalanceAfterSell = (await connection.getTokenAccountBalance(accounts[j].pubkey)).value;
+        // }
+
+        // if (accountInfo) {
+        console.log('Sol Tranfer Instruction');
         ixs.push(
           SystemProgram.transfer({
-            fromPubkey: kp.publicKey,
+            fromPubkey: wallet.publicKey,
             toPubkey: mainKp.publicKey,
-            lamports: solBal,
+            lamports: solBalance,
           }),
         );
-      }
+        // }
+        // }
 
-      if (ixs.length) {
-        const tx = new Transaction().add(
-          ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 300_000 }),
-          ComputeBudgetProgram.setComputeUnitLimit({ units: 40_000 }),
-          ...ixs,
-        );
-        tx.feePayer = mainKp.publicKey;
-        tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        if (ixs.length) {
+          const tx = new Transaction().add(
+            ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 300_000 }),
+            ComputeBudgetProgram.setComputeUnitLimit({ units: 40_000 }),
+            ...ixs,
+          );
+          tx.feePayer = mainKp.publicKey;
 
-        const simResult = await connection.simulateTransaction(tx);
-        if (simResult.value.err) {
-          console.log('Transaction simulation failed');
-          console.log('Error : ', simResult);
-          return;
-        }
+          let retry = 0;
+          while (retry < 5) {
+            try {
+              tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
 
-        const sig = await sendAndConfirmTransaction(connection, tx, [mainKp, kp], { commitment: 'confirmed' });
-        console.log(`Closed and gathered SOL from wallets ${i} : https://solscan.io/tx/${sig}`);
-        return;
-      }
+              const simResult = await connection.simulateTransaction(tx);
 
-      // filter the keypair that is completed (after this procedure, only keypairs with sol or ata will be saved in data.json)
-      const bal = await connection.getBalance(kp.publicKey);
-      if (bal == 0) {
-        const tokenAccounts = await connection.getTokenAccountsByOwner(
-          kp.publicKey,
-          {
-            programId: TOKEN_PROGRAM_ID,
-          },
-          'confirmed',
-        );
-        if (tokenAccounts.value.length == 0) {
-          await sleep(3000);
-          const walletsData = readJson();
-          const wallets = walletsData.filter(({ privateKey }) => base58.encode(kp.secretKey) != privateKey);
-          saveNewFile(wallets);
-          console.log('saved ==> ');
+              if (simResult.value.err) {
+                console.log('simResult:', simResult);
+                throw new Error('Transaction simulation failed');
+              }
+
+              const sig = await sendAndConfirmTransaction(connection, tx, [mainKp, wallet], {
+                commitment: 'confirmed',
+              });
+              console.log(`Gathered SOL from ${wallet.publicKey.toBase58()} : https://solscan.io/tx/${sig}`);
+              break;
+            } catch (error) {
+              retry++;
+              await sleep(1000);
+            }
+          }
         }
       }
     } catch (error) {
@@ -176,4 +224,4 @@ const main = async () => {
   });
 };
 
-main();
+gather();
